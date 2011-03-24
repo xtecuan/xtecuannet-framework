7d2426c911b725023e8f1c8fab9344188d474d52
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2011-03-24 06:13:22 +0000

    xtecuannet-framework-model, Initial Commit

---
+++ xtecuannet-framework-model/src/com/xtesoft/xtecuannet/framework/model/services/GenericService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xtesoft.xtecuannet.framework.model.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author xtecuan
+ */
+public interface GenericService<T> extends Serializable {
+
+    public T create(T entity);
+
+    public T edit(T entity);
+
+    public void remove(Object entity);
+
+    public T find(Object entityId);
+
+    public List<T> findAll();
+
+    public List<T> findRange(int[] range);
+
+    public int count();
+
+    public String sayHello();
+
+//    public List findByExample(Object entity,String[] excludeProperties);
+}
---
+++ xtecuannet-framework-model/src/com/xtesoft/xtecuannet/framework/utils/Configurator.java
@@ -0,0 +1,63 @@
+package com.xtesoft.xtecuannet.framework.utils;
+
+/**
+ * Clase encargada de obtener la configuraciÃ³n para el mÃ³dulo de Reportes
+ * @author <a href="mailto:xtecuan@gmail.com">Ing. Julian Rivera Pineda</a>
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Clase encargada de obtener la configuración para el módulo de Reportes
+ * @author <a href="mailto:xtecuan@gmail.com">Ing. Julian Rivera Pineda</a>
+ */
+public final class Configurator {
+    private static Log logger = LogFactory.getLog(Configurator.class);
+    private static String BUNDLE_CONFIG_CLASSPATH_PROPS="/configuration.properties";
+    public static final Properties config1 = obtenerConfiguracionProps();
+
+    public static final String obtenerEntradaConfig(String key) {
+
+        //return config.getString(key);
+        return config1.getProperty(key);
+
+    }
+
+    public static final Properties obtenerConfiguracionProps() {
+
+        Properties props = new Properties();
+        InputStream is =
+            Configurator.class.getResourceAsStream(BUNDLE_CONFIG_CLASSPATH_PROPS);
+
+
+        try {
+            props.load(is);
+
+        } catch (IOException e) {
+            logger.error("Error al llenar el archivo de configuraciÃ³n del CSMS: ",
+                         e);
+        }
+        //finally {
+
+            return props;
+
+       // }
+
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(Configurator.obtenerEntradaConfig("reports_path"));
+        //System.out.println(FilenameUtils.getBaseName("reporte_all_sec_users.jrxml"));
+
+    }
+
+
+}
