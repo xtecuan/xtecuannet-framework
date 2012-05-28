1dc44b6c29d4bc80df08c5ec7f74f1ffaba8cf78
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2012-05-28 16:00:13 +0000

    trunk/xtecuannet-framework-viewcontroller

---
+++ trunk/xtecuannet-framework-viewcontroller/src/com/xtesoft/xtecuannet/framework/viewcontroller/annotations/XFacesMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xtesoft.xtecuannet.framework.viewcontroller.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author xtecuan
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface XFacesMessage {
+
+    String message();
+}
