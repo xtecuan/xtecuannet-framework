773e37f8fdd658ed4c536034127aa08950043be5
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2012-08-30 17:54:26 +0000

    Cambios

--- trunk/ejemplosJavaEE/ClienteDirectorioApp/src/clientedirectorioapp/ClienteDirectorioApp.java
+++ trunk/ejemplosJavaEE/ClienteDirectorioApp/src/clientedirectorioapp/ClienteDirectorioApp.java
@@ -17,16 +17,26 @@ public class ClienteDirectorioApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        AgendaDTO dto = crearAgenda("Banco Agricola", "22999989", "foca@foca.com.sv", null);
-        
-        System.out.println("id: "+dto.getId());
-        
+
+        AgendaDTO dto = crearAgenda("Ejemplo Institucion", "22999989", "foca@foca.com.sv", null);
+
+        System.out.println("id: " + dto.getId());
+
+        Long result = actualizarAgenda(Long.valueOf("3"), "csj.gob.sv", null, null, Integer.valueOf("1"));
+
+        System.out.println("resultado act: " + result);
+
     }
 
     private static AgendaDTO crearAgenda(java.lang.String institucion, java.lang.String telefono, java.lang.String correo, java.lang.Integer estado) {
         org.xtecuan.modelo.ejb.AgendaFacadeService service = new org.xtecuan.modelo.ejb.AgendaFacadeService();
         org.xtecuan.modelo.ejb.AgendaFacade port = service.getAgendaFacadePort();
         return port.crearAgenda(institucion, telefono, correo, estado);
+    }
+
+    private static Long actualizarAgenda(java.lang.Long idUp, java.lang.String institucionUp, java.lang.String telefonoUp, java.lang.String correoUp, java.lang.Integer estadoUp) {
+        org.xtecuan.modelo.ejb.AgendaFacadeService service = new org.xtecuan.modelo.ejb.AgendaFacadeService();
+        org.xtecuan.modelo.ejb.AgendaFacade port = service.getAgendaFacadePort();
+        return port.actualizarAgenda(idUp, institucionUp, telefonoUp, correoUp, estadoUp);
     }
 }
