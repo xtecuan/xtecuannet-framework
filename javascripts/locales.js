b0115c02d025d9d3fe95b30107789e9893fde904
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2013-04-23 17:02:54 +0000

    Script de locales

---
+++ javascripts/locales.js
@@ -0,0 +1,13 @@
+var locales = java.util.Locale.getISOCountries();
+ 
+	for (var i=0;i<locales.length;i++) {
+ 
+      var countryCode = locales[i];
+		var obj = new java.util.Locale("", countryCode);
+ 
+		println("Country Code = " + obj.getCountry() 
+			+ ", Country Name = " + obj.getDisplayCountry());
+ 
+	}
+ 
+	println("Done");
