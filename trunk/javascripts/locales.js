4f2c65dc069593039cff7078beb14babdd9e0e30
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2013-04-23 17:04:22 +0000

    ya

--- trunk/javascripts/locales.js
+++ trunk/javascripts/locales.js
@@ -1,8 +1,8 @@
-var locales = java.util.Locale.getISOCountries();
+	var locales = java.util.Locale.getISOCountries();
  
 	for (var i=0;i<locales.length;i++) {
  
-      var countryCode = locales[i];
+     		var countryCode = locales[i];
 		var obj = new java.util.Locale("", countryCode);
  
 		println("Country Code = " + obj.getCountry() 
