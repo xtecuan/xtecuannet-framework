bddc45c01771b0a7dcc38dc68da3acc084ed53aa
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2013-07-10 23:02:05 +0000

    Xtecuannet Framework por Modulos inicialmente:
    xtecuannet-model-utils-mvn (utilitarios de modelo de datos)
    primefaces-utils-springframework (utilitarios para primefaces y springframework)   
    xtecuannet-viewcontroller-managedbeans (managed beans genericos para usar en nuestras app web)
    xtecuannet-model-jasperreports-mvn (Servicio de reporteria con jasperreports)
    xtecuannet-viewcontroller-utils (Utilitarios para vistacontrolador para jsf)

---
+++ mvn/xtecuannet-framework-modules/xtecuannet-model-jasperreports-mvn/src/main/java/com/googlecode/xtecuannet/framework/model/services/ReportsService.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.googlecode.xtecuannet.framework.model.services;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ *
+ * @author xtecuan
+ */
+public interface ReportsService {
+
+    public static final String DOC = ".doc";
+    public static final String RTF = ".rtf";
+    public static final String PDF = ".pdf";
+    public static final String REPORTS_PATH = "reports_path";
+    public static final String SUBREPORT_DIR = "SUBREPORT_DIR";
+
+    /**
+     *
+     * @return arreglo de byte[] con el reporte
+     * @param extension
+     * @param nombre
+     */
+    public byte[] generarReporteSinParametros(String nombre, String extension);
+
+    /**
+     * 
+     * @param nombre
+     * @param extension
+     * @param data
+     * @return
+     */
+    public byte[] generarReporteSinParametros(String nombre, String extension, Collection data);
+
+    /**
+     *
+     * @return arreglo de byte[] con el reporte
+     * @param pParametrosReporte
+     * @param extension
+     * @param nombre
+     */
+    public byte[] generarReporteConParametros(String nombre, String extension, Map pParametrosReporte);
+
+    /**
+     * 
+     * @param nombre
+     * @param extension
+     * @param pParametrosReporte
+     * @param data
+     * @return
+     */
+    public byte[] generarReporteConParametros(String nombre, String extension, Map pParametrosReporte, Collection data);
+}
