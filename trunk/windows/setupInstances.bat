0d13ebdae10270f79300b82cc6a1960accfd71ca
xtecuan@gmail.com <xtecuan@gmail.com@0e5b1eee-28b7-cbc4-d091-e5236fcaa726>
2014-01-12 07:03:26 +0000

    Crear directorios de instancias de Tomcat7

---
+++ trunk/windows/setupInstances.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Creating instances Directory
+mkdir %CATALINA_HOME%\instances
+echo Creating instance1 Directory
+mkdir %CATALINA_HOME%\instances\instance1
+mkdir %CATALINA_HOME%\instances\instance1\bin
+mkdir %CATALINA_HOME%\instances\instance1\conf
+mkdir %CATALINA_HOME%\instances\instance1\lib
+mkdir %CATALINA_HOME%\instances\instance1\logs
+mkdir %CATALINA_HOME%\instances\instance1\temp
+mkdir %CATALINA_HOME%\instances\instance1\webapps
+mkdir %CATALINA_HOME%\instances\instance1\work
+
+
+echo Creating instance2 Directory
+mkdir %CATALINA_HOME%\instances\instance2
+mkdir %CATALINA_HOME%\instances\instance2\bin
+mkdir %CATALINA_HOME%\instances\instance2\conf
+mkdir %CATALINA_HOME%\instances\instance2\lib
+mkdir %CATALINA_HOME%\instances\instance2\logs
+mkdir %CATALINA_HOME%\instances\instance2\temp
+mkdir %CATALINA_HOME%\instances\instance2\webapps
+mkdir %CATALINA_HOME%\instances\instance2\work
